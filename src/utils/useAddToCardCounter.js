dcc5d7e9293a836b1fba24d34796d0d5b7b61e01
baibhav17 <baibhavsxn.035@gmail.com>
2024-05-20 09:44:50 +0530

    minor issue fixed

--- src/utils/useAddToCardCounter.js
+++ src/utils/useAddToCardCounter.js
@@ -4,12 +4,12 @@ import { dataContext } from '../App';
 const useAddToCardCounter = () => {
    const [count, setCount] = useState(0);
 //    const [cartValue, setCartValue] = useState(0)
-   const {cartCountVal, setCartCountVal } =useContext(dataContext);
+//    const {cartCountVal, setCartCountVal } =useContext(dataContext);
     
     const increment = () => {
         setCount(count+1);
-        setCartCountVal(cartCountVal + 1)
-        console.log(cartCountVal)
+        // setCartCountVal(cartCountVal + 1)
+        // console.log(cartCountVal)
         // setCartValue(cartValue+1)
     }
 
